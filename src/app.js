fa236b4f37809c7b52a1c7e1f1c19f90a1a3e9bc
Gael Vivas <gaelvn2020@outlook.com>
2024-10-19 15:05:20 -0600

    create read projects

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 // Express conf
-import express from 'express';
+import express from "express";
+import projectRoutes from "./routes/projects.routes.js";
 
 const app = express();
+
+// Middlewares
+app.use(express.json()); // for parsing application/json
+
+app.use(projectRoutes);
 
 export default app;
