f25f08307b9fb61b4bdc0de07455a960bf37868a
Gael Vivas <gaelvn2020@outlook.com>
2024-10-19 20:53:25 -0600

    project update delete

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -9,6 +9,22 @@ export const getProjects = async (req, res) => {
   }
 };
 
+export const getProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const project = await Project.findOne({ where: { id } });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
 export const createProject = async (req, res) => {
   try {
     const { name, priority, description } = req.body;
@@ -20,6 +36,47 @@ export const createProject = async (req, res) => {
     });
 
     res.json(newProject);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, priority, description } = req.body;
+
+    const project = await Project.findByPk(id);
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    project.name = name;
+    project.priority = priority;
+    project.description = description;
+
+    await project.save();
+
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const project = await Project.findByPk(id);
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    await project.destroy();
+
+    res.json({ message: "Project deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
